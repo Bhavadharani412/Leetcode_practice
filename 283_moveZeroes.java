1b8be3fbb719a7f80f96613b1c702dbe733f920f
Bhavadharani <bhavadharanik412@gmail.com>
2025-04-16 13:57:44 +0530

    Create 283_moveZeroes.java

---
+++ 283_moveZeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] moveZeroes(int[] nums) {
+        int l=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]!=0){
+                int temp=nums[r];
+                nums[r]=nums[l];
+                nums[l]=temp;
+                l++;
+            }
+        } 
+        return nums; 
+    }
+}
