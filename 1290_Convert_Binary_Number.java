92d57905b4a60ec78778c26b50fb84ff393d2c66
Bhavadharani <bhavadharanik412@gmail.com>
2025-07-14 19:40:00 +0530

    Create 1290_Convert_Binary_Number.java

---
+++ 1290_Convert_Binary_Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int decimalValue = 0;
+        ListNode current = head;
+        while (current != null) {
+            decimalValue = (decimalValue << 1) | current.val;
+            current = current.next;
+        }
+        return decimalValue;
+    }
+}
