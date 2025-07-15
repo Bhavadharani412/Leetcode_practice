b3fea5a153a104a448b1728144a3e206f377d3fd
Bhavadharani <bhavadharanik412@gmail.com>
2025-07-15 10:49:05 +0530

    Create 2114_Maximum_Number_Words.java

---
+++ 2114_Maximum_Number_Words.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mostWordsFound(String[] sen) {
+        int max=0;
+        for(String s:sen)
+        {
+            int count=s.split(" ").length;
+            if(count>max)
+            {
+                max=co;
+            }
+        }
+        return max;
+        
+    }
+}
