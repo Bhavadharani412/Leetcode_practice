5f56cb1fb9073b7effef1d1848d62804523ae107
Bhavadharani <bhavadharanik412@gmail.com>
2025-08-21 10:54:04 +0530

    Create 896_Monotonic_Array.java

---
+++ 896_Monotonic_Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean asc = true;
+        boolean desc = true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                asc = false;
+            }
+            if (nums[i] < nums[i + 1]) {
+                desc = false;
+            }
+        }
+        return desc || asc;
+    }
+}
