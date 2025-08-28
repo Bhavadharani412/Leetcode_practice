b39a4e59d2353a2389444bd64656d1e60c930ea3
Bhavadharani <bhavadharanik412@gmail.com>
2025-08-28 14:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 2.4 MB (1.96%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rs
@@ -1,15 +1,9 @@
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut l = 0;
-        let mut r = nums.len() - 1;
-        while l < r {
-            let sum = nums[l] + nums[r];
-            if sum == target {
-                return vec![(l + 1) as i32, (r + 1) as i32]; 
-            } else if sum < target {
-                l += 1;
-            } else {
-                r -= 1; 
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, num) in numbers.iter().enumerate() {
+            let other = target - num;
+            if let Ok(j) = numbers[i + 1..].binary_search(&other) {
+                return vec![i as i32 + 1, (i + j) as i32 + 2];
             }
         }
         vec![]
