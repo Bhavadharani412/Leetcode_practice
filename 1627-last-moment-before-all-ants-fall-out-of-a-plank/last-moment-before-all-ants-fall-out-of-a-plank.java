2c64029ca0e7ba5a4a26b143224aa6113f25f879
Bhavadharani <bhavadharanik412@gmail.com>
2025-09-21 17:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (48.92%) - LeetSync

---
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int max = 0;
+        for (int i : left) {
+            max = Math.max(max, i);
+        }
+        for (int i : right) {
+            max = Math.max(max, n - i);
+        }  
+        return max;
+    }
+}
