59526d18fb4e68f77569b6d3052e1c5ddf17c2bd
Bhavadharani <bhavadharanik412@gmail.com>
2025-09-21 18:20:54 +0530

    Time: 1 ms (100.00%) | Memory: 61.9 MB (56.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -1,21 +1,16 @@
 class Solution {
     public int countPermutations(int[] complexity) {
-        int MOD = (int)1e9 + 7;
+        final int MODULO = 1_000_000_007;
         int n = complexity.length;
-        int mini = Integer.MAX_VALUE;
-        for (int num : complexity) {
-            mini = Math.min(mini, num);
-        }
-        if (complexity[0] != mini) return 0;
-        int cnt = 0;
-        for (int num : complexity) {
-            if (num == mini) cnt++;
-            if (cnt > 1) return 0;
+        for(int i = 1; i < n; i++){
+            if(!(complexity[0] < complexity[i])){
+                return 0;
+            }
         }
         long ans = 1;
-        for (int i = 1; i < n; i++) {
-            ans = (ans * i) % MOD;
+        for(int i = 2; i< n; i++){
+            ans = (ans * i) % MODULO;
         }
-        return (int)ans;
+        return (int) ans;
     }
 }
