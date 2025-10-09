426ad015c0ca92818d63e3939a8a058521719f2a
Bhavadharani <bhavadharanik412@gmail.com>
2025-10-09 09:35:50 +0530

    Time: 37 ms (87.56%) | Memory: 54.3 MB (16.76%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,13 @@
+var expect = function(val) {
+    function toBe(val1){
+        if ( val === val1) {
+            return true
+        }else throw new Error ("Not Equal")
+    }
+    function notToBe(val1){
+        if ( val !== val1) {
+            return true 
+        }else throw new Error ("Equal")
+    }
+    return { toBe, notToBe}    
+};
