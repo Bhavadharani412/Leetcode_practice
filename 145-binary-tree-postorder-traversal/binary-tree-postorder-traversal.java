89fda2e812a6655e6fbcea980d96061ff9266204
Bhavadharani <bhavadharanik412@gmail.com>
2025-10-16 17:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (68.07%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+       List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+    public void postorder(TreeNode root, List<Integer> res){
+        if(root != null){
+            postorder(root.left, res); 
+            postorder(root.right, res);
+            res.add(root.val);
+        }
+    } 
+}
