553dc5f959a0cb76a19e322ac1ba2747ed83aff1
Bhavadharani <bhavadharanik412@gmail.com>
2025-12-28 21:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (52.94%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.rs
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.rs
@@ -1,12 +1,23 @@
 impl Solution {
     pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
+        let rows = grid.len();
+        if rows == 0 {
+            return 0;
+        }
+        let cols = grid[0].len();
+        if cols == 0 {
+            return 0;
+        }
+        let mut r = 0;
+        let mut c = cols as i32 - 1;
         let mut count = 0;
-        for row in grid {
-            for num in row {
-                if num < 0 {
-                    count += 1;
-                }
-            }
+        while (r as usize) < rows && c >= 0 {
+            if grid[r as usize][c as usize] < 0 {
+                count += (rows - (r as usize)) as i32;
+                c -= 1;
+            } else{
+                r += 1;
+            } 
         }
         count
     }
