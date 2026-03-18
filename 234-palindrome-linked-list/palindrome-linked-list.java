52eea4c4fd5a6f8ed0b688fe409d86a8380e337d
Bhavadharani <bhavadharanik412@gmail.com>
2026-03-18 09:51:30 +0530

    Time: 3 ms (99.81%) | Memory: 94.3 MB (76.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,23 +1,20 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-       if(head == null || head.next == null) return true;
-        ListNode slow = head , fast = head; 
-        while( fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode curr = slow , prev = null;
-        while( curr != null){
-            ListNode next = curr.next;
+     // find mid point, 
+     // reverse the second half
+     // compare the first and second half
+        ListNode slow, fast;
+        for(slow = head, fast = head; fast != null && fast.next != null;slow = slow.next, fast = fast.next.next);
+        ListNode curr = slow, prev = null, next = null;
+        while(curr != null){
+            next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
-        ListNode left = head , right= prev;
-        while (right != null) {
-            if (left.val != right.val) return false;
-            left = left.next;
-            right = right.next;
+        ListNode right, left;
+        for(right = head, left = prev; left != null; right = right.next, left = left.next){
+            if(right.val != left.val) return false;
         }
         return true;
     }
