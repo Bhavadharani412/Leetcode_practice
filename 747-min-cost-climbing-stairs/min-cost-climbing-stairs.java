94d50a42442388545b76834b4152cfc13c140050
Bhavadharani <bhavadharanik412@gmail.com>
2026-04-21 15:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (43.01%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -5,7 +5,8 @@ class Solution {
         mincost[0] = cost[0];
         mincost[1] = cost[1];
         for(int iter = 2; iter < len; iter++){
-            mincost[iter]=Math.min(mincost[iter-2],mincost[iter-1])+cost[iter];
+            mincost[iter] = cost[iter];
+            mincost[iter] += Math.min(mincost[iter-2],mincost[iter-1]);
         }
         return Math.min(mincost[len - 1], mincost[len-2]);
     }
