b8295e8a46e23c96dc25a39adc8697cb81d26ca9
Bhavadharani <bhavadharanik412@gmail.com>
2026-05-07 22:43:12 +0530

    Time: 4 ms (100.00%) | Memory: 198.5 MB (66.13%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.java
+++ 3981-jump-game-ix/jump-game-ix.java
@@ -1,28 +1,13 @@
 class Solution {
     public int[] maxValue(int[] nums) {
-        int n = nums.length;
-        int[] pre = new int[n];
-        int[] suf = new int[n];
-        int[] res = new int[n];
-        pre[0] = nums[0];
-        for (int i = 1; i < n; i++) 
-        {
-            pre[i] = Math.max(pre[i - 1], nums[i]);
+        int n = nums.length, min = Integer.MAX_VALUE;
+        int[] max = new int[n], ans = new int[n];
+        max[0] = nums[0];
+        for(int i = 1; i < n; i++) max[i] = Math.max(max[i - 1], nums[i]);
+        for(int i = n - 2; i >= 0; i--) {
+            min = Math.min(min, nums[i + 1]);
+            if(min < max[i]) max[i] = max[i + 1];
         }
-        suf[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) 
-        {
-            suf[i] = Math.min(suf[i + 1], nums[i]);
-        }
-        res[n - 1] = pre[n - 1];
-        for (int i = n - 2; i >= 0; i--) 
-        {
-
-            if (pre[i] > suf[i + 1]) 
-                res[i] = res[i + 1];
-            else 
-                res[i] = pre[i];
-        }
-        return res;
+        return max;
     }
 }
