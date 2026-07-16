e978df8a2c434136ca58b89b86a50f2fe3325ab3
Bhavadharani <bhavadharanik412@gmail.com>
2026-07-16 22:25:07 +0530

    Time: 7 ms (52.27%) | Memory: 57.8 MB (54.51%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,11 +1,20 @@
+/*
+*Traverse the array once, using each value arr[i] to compute an index idx = abs(arr[i]) - 1.
+Check the value at arr[idx]:
+=> If it is positive, mark it as visited by setting arr[idx] = -arr[idx].
+=> If it is already negative, it means arr[i] has been seen before, so it's a duplicate.
+Add such duplicate values to the result list
+*/
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            int idx = Math.abs(nums[i]) - 1;
-            if(nums[idx] < 0) ans.add(Math.abs(idx+1));
-            nums[idx] = -nums[idx];
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        for(int ind = 0; ind < n ; ind++)
+        {
+            int val = Math.abs(nums[ind]) - 1;
+            if(nums[val]< 0) res.add(Math.abs(nums[ind]));
+            else nums[val] = -nums[val];
         }
-        return ans;
+        return res;
     }
 }
