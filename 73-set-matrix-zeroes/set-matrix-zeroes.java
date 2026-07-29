8ed28afc07d9f3c286abe86d4ad7adaa575d4b17
Bhavadharani <bhavadharanik412@gmail.com>
2026-07-29 10:59:30 +0530

    Time: 1 ms (97.70%) | Memory: 47.6 MB (62.59%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean fir_row = false, fir_col = false;
+        for(int i = 0; i < n; i++)
+        {
+            if(matrix[0][i] == 0) fir_row = true;
+        }
+        for(int i = 0; i < m; i++)
+        {
+            if(matrix[i][0] == 0) fir_col = true;
+        }
+        // marker 
+        for(int i =1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        // fill karo
+        for (int i = 1; i < m; i++) 
+        {
+            for (int j = 1; j < n; j++) 
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) 
+                    matrix[i][j] = 0;
+            }
+        }
+        if(fir_row)
+        {
+            for(int i = 0; i < n; i++) matrix[0][i] = 0;
+        }
+        if(fir_col) for(int i = 0; i < m; i++) matrix[i][0] = 0;
+    }
+}
