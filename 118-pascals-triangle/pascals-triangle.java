52d3ae86b09fe96136f30e4f3226cf73ceec7b96
Bhavadharani <bhavadharanik412@gmail.com>
2026-07-29 13:45:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (52.62%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,36 @@
 class Solution {
-    private List<Integer> generateRow(int row) {
-        List<Integer> ans = new ArrayList<>(Collections.nCopies(row + 1, 1));
-        long val = 1;
-        int mid = row / 2;
-        for (int r = 1; r <= mid; r++) {
-            val = val * (row - r + 1);
-            val /= r;
-            ans.set(r, (int) val);
-            ans.set(row - r, (int) val);
+    public List<List<Integer>> generate(int numRows) {
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     for(int i=0;i<numRows;i++){
+    //         ans.add(new ArrayList<Integer>());
+    //         for(int j=0;j<=i;j++){
+    //             if(j==0 || j==i){
+    //                  ans.get(i).add(j,1);
+    //         }
+    //         else{
+    //             int val = ans.get(i-1).get(j)+ans.get(i-1).get(j-1);
+    //             ans.get(i).add(j,val);
+    //         }
+    //     }
+    // }
+    // return ans;
+    List<List<Integer>> list = new ArrayList<>();
+    for(int row=1;row<=numRows;row++){
+        List<Integer> b = new ArrayList<>();
+        list.add(generateRow(row));
+    }
+    return list;
+
+    }
+    public List<Integer> generateRow(int n){
+        List<Integer> ans = new ArrayList<>();
+        int a = 1;
+        ans.add(1);
+        for(int i=1;i<n;i++){
+            a = a*(n-i);
+            a = a/i;
+            ans.add(a);
         }
         return ans;
-    }
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int row = 0; row < numRows; row++) {
-            res.add(generateRow(row));
-        }
-        return res;
     }
 }
