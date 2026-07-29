70e71c9144076ffb6417e22989b74ca98a4bdfb5
Bhavadharani <bhavadharanik412@gmail.com>
2026-07-29 13:45:11 +0530

    Time: 1 ms (94.28%) | Memory: 43.2 MB (94.09%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,16 +1,21 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
-            int mid = i / 2;
-            for (int j = 1; j <= mid; j++) {
-                int val = ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j);
-                row.set(j, val);
-                row.set(i - j, val); // mirror
-            }
-            ans.add(row);
+    private List<Integer> generateRow(int row) {
+        List<Integer> ans = new ArrayList<>(Collections.nCopies(row + 1, 1));
+        long val = 1;
+        int mid = row / 2;
+        for (int r = 1; r <= mid; r++) {
+            val = val * (row - r + 1);
+            val /= r;
+            ans.set(r, (int) val);
+            ans.set(row - r, (int) val);
         }
         return ans;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int row = 0; row < numRows; row++) {
+            res.add(generateRow(row));
+        }
+        return res;
     }
 }
